42c686b871c6d594757a404f020d8a3d196bd40c
Marco Nucara <marco.nucara@gmail.com>
2021-06-03 18:45:32 +0200

    fix ts

--- packages/website/pages/components/Navbar.tsx
+++ packages/website/pages/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Navbar(props) {
+export default function Navbar(props: { transparent: any }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
