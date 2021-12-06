5af9cd26ce47ef29e99560a7876d44ebc49fd15b
Marco Nucara <marco.nucara@gmail.com>
2021-12-06 10:17:11 +0000

    fix robots

--- packages/website/next-sitemap.js
+++ packages/website/next-sitemap.js
@@ -2,10 +2,7 @@ module.exports = {
   siteUrl: `https://www.gianlucasantambrogio.com`,
   generateRobotsTxt: true, // (optional)
   robotsTxtOptions: {
-    policies:
-      process.env.NEXT_PUBLIC_STAGE === "production"
-        ? [{ userAgent: "*", allow: "/" }]
-        : [{ userAgent: "*", disallow: "/" }],
+    policies: [{ userAgent: "*", allow: "/" }],
   },
   priority: 0.1,
   transform: async (config, path) => {
